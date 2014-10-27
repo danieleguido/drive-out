5aa8baa83a340283598ba402568a8b4aa1e0bec7
Daniele Guido <gui.daniele@gmail.com>
2014-10-27 20:01:32 +0100

    basic style modifications

--- app/scripts/controllers/depth.js
+++ app/scripts/controllers/depth.js
@@ -11,15 +11,21 @@ angular.module('driveoutApp')
   .controller('DepthCtrl', function ($scope, items, parents, $log, $routeParams) {
     $scope.items = items.data;
     $scope.$parent.folders = angular.copy(parents.data).filter(function(d) {
+      if(d.slug==$routeParams.sub)
+        d.selected = true;
       if(d.type=='folder')
         return d;
     });
+
+    // that is the selected folder
+    //$cope.$parent.folder = 
 
     // filter stuffs
     $scope.$watch('routes', function(){ // get title
       $scope.$parent.page = angular.copy($scope.routes)
         .filter(function(d){ return d.slug==$routeParams.page })
         .pop();
+      //$scope.$parent.page.depth 
       $log.info('routes loaded', $scope.page)
     })
   });
